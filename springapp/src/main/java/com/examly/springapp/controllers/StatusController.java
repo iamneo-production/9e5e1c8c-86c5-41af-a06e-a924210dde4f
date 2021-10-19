83be7a53725aa931b506e06807810113fee607c1
4ttacktitan <pratyaydeep@yahoo.com>
2021-10-19 20:15:03 +0000

    Modified ComplaintController

--- springapp/src/main/java/com/examly/springapp/controllers/StatusController.java
+++ springapp/src/main/java/com/examly/springapp/controllers/StatusController.java
@@ -1,13 +1,14 @@
 package com.examly.springapp.controllers;
 import java.util.List;
 
-	import org.springframework.beans.factory.annotation.Autowired;
-	import org.springframework.http.HttpStatus;
-	import org.springframework.http.ResponseEntity;
-	import org.springframework.web.bind.annotation.DeleteMapping;
-	import org.springframework.web.bind.annotation.GetMapping;
-	import org.springframework.web.bind.annotation.PathVariable;
-	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 	
 
 	import com.examly.springapp.models.StatusModel;
@@ -21,16 +22,36 @@ import java.util.List;
 		
 		public StatusController(StatusService statusservice) {	this.statusservice = statusservice;}
 		
-		@GetMapping("/openstatus")
+		@GetMapping("/admin/openStatus")
 		public ResponseEntity<List<StatusModel>> showOpenStatus(){
 			List<StatusModel> status=statusservice.showOpenStatus();
 			return new ResponseEntity<>(status,HttpStatus.OK);		
 		}
 		
-		@GetMapping("/closedstatus")
+		@GetMapping("/admin/closedStatus")
 		public ResponseEntity<List<StatusModel>> showClosedStatus(){
 			List<StatusModel> status=statusservice.showClosedStatus();
 			return new ResponseEntity<>(status,HttpStatus.OK);		
 		}
 		
+		@PutMapping("/status/{statusId}")
+		public ResponseEntity<StatusModel> userUpdateStatus(@RequestBody StatusModel status, @PathVariable int statusId){
+			status.setStatusId(statusId);
+			statusservice.updateStatus(status);
+			return new ResponseEntity<>(status,HttpStatus.CREATED);
+		}
+		
+		@PutMapping("/employee/status/{statusId}")
+		public ResponseEntity<StatusModel> employeeUpdateStatus(@RequestBody StatusModel status, @PathVariable int statusId){
+			status.setStatusId(statusId);
+			statusservice.updateStatus(status);
+			return new ResponseEntity<>(status,HttpStatus.CREATED);
+		}
+		
+		@PutMapping("/admin/status/{statusId}")
+		public ResponseEntity<StatusModel> adminUpdateStatus(@RequestBody StatusModel status, @PathVariable int statusId){
+			status.setStatusId(statusId);
+			statusservice.updateStatus(status);
+			return new ResponseEntity<>(status,HttpStatus.CREATED);
+		}	
 	}
