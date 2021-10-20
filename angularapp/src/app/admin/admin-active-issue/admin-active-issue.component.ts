ff628a263228430a2f27b7a1af052a88eeccfcfa
saha-bhaswati <saha.bhaswati2000@gmail.com>
2021-10-20 15:07:35 +0000

    Admin completed

--- angularapp/src/app/admin/admin-active-issue/admin-active-issue.component.ts
+++ angularapp/src/app/admin/admin-active-issue/admin-active-issue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { EventService } from 'src/app/event.service';
 
 @Component({
   selector: 'app-admin-active-issue',
@@ -7,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminActiveIssueComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService:AuthService, public _eventService:EventService) { }
+  private _authtoken=localStorage.getItem('Authorization');
+  public allComplaintsByAdmin=[];
+  public noUser=0;
+  public noDeveloper=0;
+  public noActive=0;
+  public noSolved=0;
 
   ngOnInit(): void {
+    this._eventService.getAllComplaintsByAdmin(this._authtoken).subscribe(
+      res => {
+        this.allComplaintsByAdmin=res;
+        for(let u in res){
+          if(res[u]['status']['status']==='Active'){
+            this.noActive++;
+          }
+          if(res[u]['status']['status']==='Closed'){
+            this.noSolved++;
+          }
+        }
+
+      },
+      err =>{
+        console.log(err);
+
+      }
+
+    )
+    this._eventService.getAllUsers(this._authtoken).subscribe(
+      res => {
+        for(let u in res){
+          if(res[u]['role']==='ROLE_USER'){
+            this.noUser++;
+          }
+          if(res[u]['role']==='ROLE_EMPLOYEE'){
+            this.noDeveloper++;
+          }
+
+        }
+      },
+      err => {
+        console.log(err);
+
+      }
+    )
   }
-  
 
 }
