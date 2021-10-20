c6ffed798a64c36b06ce814007d7a7e084562d1a
sourasishc <sayan.roy4343@gmail.com>
2021-10-20 15:45:59 +0000

    Implemented Employee

--- angularapp/src/app/employee/edit-status-by-id/edit-status-by-id.component.ts
+++ angularapp/src/app/employee/edit-status-by-id/edit-status-by-id.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+import { EventService } from 'src/app/event.service';
 
 @Component({
   selector: 'app-edit-status-by-id',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-status-by-id.component.css']
 })
 export class EditStatusByIdComponent implements OnInit {
-
-  constructor() { }
+  private _authtoken=localStorage.getItem('Authorization');
+  public complaint:any=[]
+  constructor(public authService:AuthService, private _service:EventService,private _activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    let id:any=this._activatedRoute.snapshot.paramMap.get('id');
+    this._service.getEachComplaintDetailByEmployee(id,this._authtoken).subscribe(
+      res => {
+        //console.log(res);
+        this.complaint=res;
 
+      },
+      err => {
+        console.log(err);
+
+      }
+    )
+  }
+  display = true;
+  onEditStatus(){
+    this.display = !this.display;
+  }
 }
